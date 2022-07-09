7caaa4ca32fb8f82ac3e1c89ed37b2f7fa29d9d9
neogmanish99 <sanjuneog399@gmail.com>
2022-07-09 10:37:38 +0530

    worked on events and prop drilling

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,7 +1,8 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
 
-const FeedbackList = ({ feedback }) => {
+const FeedbackList = ({ feedback, handleDelete }) => {
     if (!feedback || feedback.length === 0) {
         return <p>No feedback</p>;
     }
@@ -9,10 +10,24 @@ const FeedbackList = ({ feedback }) => {
     return (
         <div className="feedback-list">
             {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item} />
+                <FeedbackItem
+                    key={item.id}
+                    item={item}
+                    handleDelete={handleDelete}
+                />
             ))}
         </div>
     );
+};
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired,
+        })
+    ),
 };
 
 export default FeedbackList;
